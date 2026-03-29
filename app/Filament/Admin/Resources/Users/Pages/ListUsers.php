0cf801c1ad506d7276bc0775afb508531de5cc12
Shuki Vaknin <shukivaknin@gmail.com>
2026-03-29 23:18:52 +0300

    Split Users page into Users and Administrators tabs

--- app/Filament/Admin/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Admin/Resources/Users/Pages/ListUsers.php
@@ -7,6 +7,8 @@ namespace App\Filament\Admin\Resources\Users\Pages;
 use App\Filament\Admin\Resources\Users\UserResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListUsers extends ListRecords
 {
@@ -17,5 +19,22 @@ class ListUsers extends ListRecords
         return [
             CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'users' => Tab::make(__('Users'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('is_admin', false))
+                ->icon('heroicon-o-users'),
+            'admins' => Tab::make(__('Administrators'))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('is_admin', true))
+                ->icon('heroicon-o-shield-check'),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string|int|null
+    {
+        return 'users';
     }
 }
